959399f0e1b0c11c96c202474bfa2b256b46ff62
Artem Lipchansky <lipchanskiy.an@gmail.com>
2024-03-05 19:07:08 +0200

    added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContextZ('2d')
-c.fillReact(10, 10, 50, 50)
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1024
+canvas.height = 576
+
+class Player {
+    constructor() {
+        this.position = {
+            x: 0,
+            y: 0,
+        }
+    }
+    draw() {
+ c.fillStyle = 'red';
+ c.fillRect(this.position.x, this.position.y, 100, 100);
+    }
+
+    update() {
+        this.position.y++
+    }
+}
+
+let y = 100
+let y2 = 100
+
+function animate() {
+    window.requestAnimationFrame(animate);
+c.fillStyle = 'white';
+c.fillRect(0, 0, canvas.width, canvas.height);
+    // console.log('go');
+
+
+y++
+    
+// c.fillStyle = 'red';
+//  c.fillRect(400, y2, 100, 100);
+//  y2++
+}
+
+animate()
